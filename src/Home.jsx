7eada10f7eefce56d3fab9e41621650754406b88
vimalkarthikh <137371585+vimalkarthikh@users.noreply.github.com>
2023-08-07 14:36:18 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+
+function Home() {
+  return React.createElement('section',{className:'pricing py-5'},null,
+  React.createElement('div',{className:'container'},
+  React.createElement('div',{className:'row'},
+  
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},"Free"),
+  React.createElement('h6',{className:'card-price text-center'},"$0",
+  React.createElement('span',{className:'period'},"/month")),
+  React.createElement('hr'),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Single User"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"5GB Storage"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Unlimited Public Projects"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Community Access"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Unlimited Private Projects"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Dedicated Phone Suport"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Free Subdomain"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Monthly Status Reports")))))))))),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{className:'btn btn-primary text-uppercase'},"Button")
+  
+
+  )))),
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},"Plus"),
+  React.createElement('h6',{className:'card-price text-center'},"$9",
+  React.createElement('span',{className:'period'},"/month")),
+  React.createElement('hr'),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"5 User"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"50 GB Storage"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Unlimited Public Projects"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Community Access"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Unlimited Private Projects"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Dedicated Phone Suport"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Free Subdomain"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Monthly Status Reports")))))))))),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{className:'btn btn-primary text-uppercase'},"Button")
+  
+
+  )))),
+  
+  
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},"Pro"),
+  React.createElement('h6',{className:'card-price text-center'},"$49",
+  React.createElement('span',{className:'period'},"/month")),
+  React.createElement('hr'),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Unlimited User"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"150 GB Storage"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Unlimited Public Projects"),
+  React.createElement('li',null,
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-check'}),"Community Access"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Unlimited Private Projects"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Dedicated Phone Suport"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),React.createElement('strong',"Unlimited"),"Free Subdomain"),
+  React.createElement('li',{className:'text-muted'},
+  React.createElement('span',{className:'fa-li'},
+  React.createElement('i',{className:'fas fa-times'}),"Monthly Status Reports")))))))))),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{className:'btn btn-primary text-uppercase'},"Button")
+  
+
+  ))))
+  
+  )
+  ))
+}
+
+export default Home
